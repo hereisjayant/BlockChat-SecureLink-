58b6026a67b5f5941f74c7e521475025b4287ef0
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2022-11-30 23:58:37 -0800

    remove messenger calls and cleaning

--- src/components/MessageRoom/index.js
+++ src/components/MessageRoom/index.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Chat from "../../abis/Chat.json";
 
-import Messenger from "../../services/chat";
-
 const MessageRoom = ({
   userAddress
 }) =>  {
 
-  const [currMessenger, setCurrMessenger] = useState(null);
-  const [messengers, setMessengers] = useState({});
   const [chatContract, setChatContract] = useState(null);
   const [isListenersActive, setIsListenersActive] = useState(false);
 
@@ -110,8 +106,7 @@ const MessageRoom = ({
   const fetchAddresses = async () => {
     const addresses = await window.web3.eth.getAccounts();
     setAddresses(addresses);
-    // TODO: remove lines below
-    // await createNewMessenger();
+    // TODO: allow recipient to change
     setRecipientAddress(addresses[1]);
     return addresses;
   }
@@ -131,39 +126,14 @@ const MessageRoom = ({
     });
   }
 
-  // This is where things start going downhill
-  const createNewMessenger = async () => {
-    try {
-      const m = new Messenger();
-      console.log('new thing', m);
-      await m.generateDH();
-      let pkr = await m.getDHPublicKey();
-      await m.generateRootKey(pkr);
-      await m.generateDH();
-      pkr = await m.getDHPublicKey();
-      setCurrMessenger(m);
-      console.log({ m })
-    } catch (err) {
-      console.error("error loading messenger!", err);
-    }
-  }
-
   const testFunction = async () => {
     const testMsg = "Test message";
     console.log(`Sending test message to ${recipientAddress}: "${testMsg}"`);
     // ** CALL ENCRYPT HERE **
-    const m = new Messenger();
-    console.log('new thing', m);
-    await m.generateDH();
-    let pkr = await m.getDHPublicKey();
-    await m.generateRootKey(pkr);
-    await m.generateDH();
-    pkr = await m.getDHPublicKey();
-    // setCurrMessenger(m);
-    const testPackageEncrypted = await m.ratchetEncrypt(testMsg);
-    const { cipherText: testMsgEncrypted, ...headers } = testPackageEncrypted;
-    console.log(`### ecrpyted message: ${testMsgEncrypted}`)
-    await requestSendMessage(testMsg, JSON.stringify(headers));
+    // const testPackageEncrypted = await m.ratchetEncrypt(testMsg);
+    // const { cipherText: testMsgEncrypted, ...headers } = testPackageEncrypted;
+    // console.log(`### ecrpyted message: ${testMsgEncrypted}`)
+    // await requestSendMessage(testMsg, JSON.stringify(headers));
     // await requestGetAllMessages();
   }
 
@@ -174,7 +144,7 @@ const MessageRoom = ({
       <div>All Available Addresses:</div>
       <ul>{addresses.map((add) => <li key={add}>{String(add)}</li>)}</ul>
       <h3>Test Info</h3>
-      <div>isMessengerAvailable: {String(currMessenger !== null)}</div>
+      {/* <div>isMessengerAvailable: {String(currMessenger !== null)}</div> */}
       <div>isContractAvailable: {String(chatContract !== null)}</div>
       <div>isListenersActive: {String(isListenersActive)}</div>
       <button onClick={testFunction}>Send Test message</button>
